db06ca2b85e454cd5929ccebbce897fcd2a586a1
Fabiany Lopes <fabiany.lopes.060@ufrn.edu.br>
2022-05-16 14:15:58 -0300

    refactor

--- src/components/Auth/SignIn.js
+++ src/components/Auth/SignIn.js
@@ -23,9 +23,9 @@ function SignIn() {
   }
 
   function handleSuccess(response) {
-    //setUserInfo(response.data.customerInfo);
     ///console.log(userInfo)
     setUserInfo({...userInfo, userInfo: response.data.customerInfo});
+
     const promise = axios.put("http://localhost:5000/bag",
       {
         newCustomer_id: response.data.customer_id,
